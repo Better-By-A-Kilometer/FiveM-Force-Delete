cd3e99b12b6cfb2c8de3fee11e4bdd760d2d7fa9
Kilo <37485242+DevKilo1@users.noreply.github.com>
2025-03-17 16:26:49 -0400

    .

--- KiloForceDelete/client.lua
+++ KiloForceDelete/client.lua
@@ -7,7 +7,7 @@ function SelectNearestFromPool(poolName, position, radius)
     local entities = GetGamePool(poolName);
     local maxDistance = radius;
     local selectedPed = nil;
-    for _,handle in pairs(entities) do
+    for _, handle in pairs(entities) do
         if DoesEntityExist(handle) and not IsPedAPlayer(handle) then
             local pedCoords = GetEntityCoords(handle);
             local dist = Vdist(position.x, position.y, position.z, pedCoords.x, pedCoords.y, pedCoords.z)
@@ -20,41 +20,87 @@ function SelectNearestFromPool(poolName, position, radius)
     return DoesEntityExist(selectedPed) and selectedPed or nil;
 end
 
-function fdv(radius)
+function fdv(radius, radiusInput)
     -- Select vehicle
     local position = GetEntityCoords(GetPlayerPed(-1));
     local veh = SelectNearestFromPool("CVehicle", position, radius);
     if veh then
         DeleteEntity(veh);
     end
+    
+    if radiusInput and type(radius) == "number" then
+        -- Deletes all entities within the player-specified radius
+        local entities = GetGamePool("CVehicle");
+        local maxRadius = radius;
+        for _, handle in pairs(entities) do
+            if DoesEntityExist(handle) then
+                local pedCoords = GetEntityCoords(handle);
+                local dist = Vdist(position.x, position.y, position.z, pedCoords.x, pedCoords.y, pedCoords.z)
+                if dist < maxRadius then
+                    DeleteEntity(handle);
+                end
+            end
+        end
+    end
 end
 
-function fdo(radius)
+function fdo(radius, radiusInput)
     local position = GetEntityCoords(GetPlayerPed(-1));
     local obj = SelectNearestFromPool("CObject", position, radius);
     if obj then
         DeleteEntity(obj);
     end
+
+    if radiusInput and type(radius) == "number" then
+        -- Deletes all entities within the player-specified radius
+        local entities = GetGamePool("CObject");
+        local maxRadius = radius;
+        for _, handle in pairs(entities) do
+            if DoesEntityExist(handle) then
+                local pedCoords = GetEntityCoords(handle);
+                local dist = Vdist(position.x, position.y, position.z, pedCoords.x, pedCoords.y, pedCoords.z)
+                if dist < maxRadius then
+                    DeleteEntity(handle);
+                end
+            end
+        end
+    end
 end
 
-function fdp(radius)
+function fdp(radius, radiusInput)
     local position = GetEntityCoords(GetPlayerPed(-1));
     local ped = SelectNearestFromPool("CPed", position, radius);
     if ped then
         DeleteEntity(ped);
     end
-end
-if Commands.fdv and Commands.fdv.enabled then
-    AddEventHandler(Prefix.."fdv", fdv);
-end
-if Commands.fdo and Commands.fdo.enabled then
-    AddEventHandler(Prefix.."fdo", fdo);
-end
-if Commands.fdp and Commands.fdp.enabled then
-    AddEventHandler(Prefix.."fdp", fdp);
+
+    if radiusInput and type(radiusInput) == "number" then
+        -- Deletes all entities within the player-specified radius
+        local entities = GetGamePool("CVehicle");
+        local maxRadius = radius;
+        for _, handle in pairs(entities) do
+            if DoesEntityExist(handle) and not IsPedAPlayer(handle) then
+                local pedCoords = GetEntityCoords(handle);
+                local dist = Vdist(position.x, position.y, position.z, pedCoords.x, pedCoords.y, pedCoords.z)
+                if dist < maxRadius then
+                    DeleteEntity(handle);
+                end
+            end
+        end
+    end
 end
 
-for cmd,data in pairs(Commands) do
+if Commands.fdv and Commands.fdv.enabled then
+    AddEventHandler(Prefix .. "fdv", fdv);
+end
+if Commands.fdo and Commands.fdo.enabled then
+    AddEventHandler(Prefix .. "fdo", fdo);
+end
+if Commands.fdp and Commands.fdp.enabled then
+    AddEventHandler(Prefix .. "fdp", fdp);
+end
+
+for cmd, data in pairs(Commands) do
     local Enabled = data.enabled or false;
     local Keybind = data.keybind or false;
     local Restricted = data.restricted or false;
@@ -62,17 +108,25 @@ for cmd,data in pairs(Commands) do
     local ToolTip = data.tooltip or "";
     local DefaultKeybind = data.defaultKeybind or "";
     local Radius = data.radius or 2;
+    local RadiusInput = data.radiusInput or false;
 
     if Enabled then
-        RegisterCommand(cmd, function(source)
+        RegisterCommand(cmd, function(source, radius)
             if source < 0 then return end; -- Ignore console
-            TriggerEvent(Prefix..cmd, Radius);
+            if RadiusInput and type(radius) == "number" then
+                Radius = math.min(radius, Radius);
+            end
+            TriggerEvent(Prefix .. cmd, Radius, RadiusInput);
         end, Restricted);
         if Keybind then
             RegisterKeyMapping(cmd, Description, "keyboard", DefaultKeybind);
         end
         if ToolTip and ToolTip ~= "" then
-            TriggerEvent("chat:addSuggestion", "/"..cmd, ToolTip, {})
+            local args = {};
+            if RadiusInput then
+                args[1] = { name = "radius", help = "Deletes all peds within your specified radius" }
+            end
+            TriggerEvent("chat:addSuggestion", "/" .. cmd, ToolTip, args)
         end
     end
 end
--- KiloForceDelete/config.lua
+++ KiloForceDelete/config.lua
@@ -1,8 +1,19 @@
 Config = {
     Prefix = GetCurrentResourceName() .. "::",
     Commands = {
-        fdv = { enabled = true, keybind = true, restricted = false, description = "Deletes nearest vehicle", tooltip = "Deletes nearest vehicle", defaultKeybind = "", radius = 5 },
-        fdp = { enabled = true, keybind = true, restricted = false, description = "Deletes nearest ped", tooltip = "Deletes nearest ped", defaultKeybind = "", radius = 2 },
-        fdo = { enabled = true, keybind = true, restricted = false, description = "Deletes nearest object", tooltip = "Deletes nearest object", defaultKeybind = "", radius = 1.5 }
+        fdv = { enabled = true, keybind = true, restricted = false, description = "Deletes nearest vehicle", tooltip = "Deletes nearest vehicle", defaultKeybind = "", radius = 5, radiusInput = false },
+        fdp = { enabled = true, keybind = true, restricted = false, description = "Deletes nearest ped", tooltip = "Deletes nearest ped", defaultKeybind = "", radius = 2, radiusInput = false },
+        fdo = { enabled = true, keybind = true, restricted = false, description = "Deletes nearest object", tooltip = "Deletes nearest object", defaultKeybind = "", radius = 1.5, radiusInput = false }
     }
 };
+
+-- Leaving defaultKeybind as a blank string will not assign it to a keybind, but still allows the player to set it themselves.
+-- If radiusInput is true, a number argument called 'radius' will be added to the command. When a radius is specified by the player, the resource will delete all entities of the specified type within the specified radius. Additionally, the 'radius' you set is treated as a maximum to prevent abuse.
+-- If restricted is true, not everyone can use the command. You'll have to assign the command.fdv (or fdp or fdo) permission to the people you want to be able to use it.
+-- If keybind is false, the keybindings won't be created.
+-- If enabled is false, the command is disabled.
+-- radius is the maximum deletion radius
+-- tooltip is the description that appears when typing the command in chat
+-- description is the description that appears on the keybinding.
+ 
+-- defaultKeybind is the default keybind. Changing this will not change any existing keybinds, which are stored on the client. This means it will only affect new players.
